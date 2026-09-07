e21525e7c13b917cc10ab9c5d17639c950b1998c
tanishqgupta001 <tanishqgupta37975@gmail.com>
2026-09-07 20:03:22 +0530

    Time: 5 ms (83.09%) | Memory: 77.2 MB (68.92%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxArea(int[] height) {
         
-        int left = 0;
+      int left = 0;
         int right = height.length - 1;
         int maxArea = 0;
         
@@ -24,3 +24,5 @@ class Solution {
         return maxArea;
     }
 }
+
+  
